09950dc15a18b0af016eba9d4f7099c13cafb301
Akshay Verma <15175239+akshay-verma@users.noreply.github.com>
2019-01-20 14:04:48 +0530

    Add post on color quantization

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Coming Soon!",
-        "excerpt":" ","categories": ["jekyll","update"],
+        "title": "Color Quantization",
+        "excerpt":"Introduction Color quantization is used to reduce the number of colors that are used to represent an image. The objective is to reduce the colors without affecting the visual appearance of the original image. However, some information is lost during the process and hence it is called as a lossy...","categories": ["computer-vision"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/coming-soon/",
+        "url": "http://localhost:4000/2019/01/19/computer-vision/color-quantization/",
         "teaser":null}]
